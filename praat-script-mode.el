ef1d7576229b274eb00c47b5807b60ef0f45b43d
stefanocoretta <stefano.coretta@gmail.com>
2021-05-25 22:38:41 +0200

    📝 - add license to custom packages

--- praat-script-mode.el
+++ praat-script-mode.el
@@ -6,6 +6,28 @@
 ;;
 ;; Keywords: praat
 
+;; MIT License
+;;
+;; Copyright (c) 2021 Stefano Coretta
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 (defvar praat-script-mode-syntax-table nil
   "Syntax table in use in praat-script-mode buffers.")
 
@@ -40,6 +62,7 @@
           '("\\([[:upper:]]\\w*\\(\\w\\| \\|(\\|)\\)*\\)\\(:\\|$\\)"
             (1 font-lock-builtin-face))
 
+          ;; Keywords imported from praat-mode.el
           (list (regexp-opt '("break" "continue" "delete" "exit" "else" "for" "getline"
                               "if" "next" "print" "printf" "return" "while" "call" "clearinfo"
                               "comment" "down" "echo" "editor" "else" "elsif" "endeditor"
