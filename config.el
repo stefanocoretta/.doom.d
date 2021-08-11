04ded83ef21211401f2f45173f40e298c69dea2e
stefanocoretta <stefano.coretta@gmail.com>
2021-08-11 20:31:51 +0200

    ♻️ - move org-roam to icloud

--- config.el
+++ config.el
@@ -201,7 +201,7 @@ _j_/_k_  move up/down      _u_ keep upper            _r_ resolve
 ;; First day of the week
 (setq calendar-week-start-day 1) ; 0:Sunday, 1:Monday
 
-(setq deft-directory "/Users/ste/Library/Mobile Documents/com~apple~CloudDocs/drive/deft"
+(setq deft-directory "/Users/ste/Library/Mobile Documents/com~apple~CloudDocs/drive/roam"
       deft-extensions '("org", "md", "txt", "tex")
       deft-recursive t)
 
@@ -234,10 +234,15 @@ _j_/_k_  move up/down      _u_ keep upper            _r_ resolve
       '("~/repos" "~/repos/conlang" "~/repos/research" "~/repos/software" "~/repos/typography" "~/repos/web" "~/repos/b4ss" "~/repos/many-speech" "~/repos/teaching" "~/repos/intRo")
       projectile-auto-discover nil)
 
-(setq org-roam-directory (file-truename "/Users/ste/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org")
+(setq org-roam-directory (file-truename "/Users/ste/Library/Mobile Documents/com~apple~CloudDocs/drive/roam")
       org-id-locations-file "/Users/ste/.emacs.d/.org-id-locations"
       org-roam-capture-templates '(("d" "default" plain "%?"
                                     :if-new (file+head "${slug}.org" "#+title: ${title}\n#+date: %t\n#+filetags: \n\n")
+                                    :unnarrowed t)
+                                   ;; bibliography note template
+                                   ("r" "bibliography reference" plain "%?"
+                                    :if-new
+                                    (file+head "references/${citekey}.org" "#+title: ${title}\n")
                                     :unnarrowed t)))
 
 (use-package! org-roam-bibtex
@@ -369,7 +374,7 @@ _j_/_k_  move up/down      _u_ keep upper            _r_ resolve
   (setq org-agenda-files '("/Users/ste/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/agenda.org"))
   (setq org-agenda-span 14)
   (setq org-agenda-start-on-weekday 1)
-  (setq org-directory "/Users/ste/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org"))
+  (setq org-directory "/Users/ste/Library/Mobile Documents/com~apple~CloudDocs/drive/roam"))
 
 (add-hook 'org-agenda-finalize-hook
   (lambda ()
